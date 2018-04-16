928bad5ff0d5806712728fdb7bf1bc069a5b243c
sanpab <sanpab@LOUL140393.verit.dnv.com>
2018-04-16 15:48:59 +0100

    added line 4

--- JavaHelloWorld/src/HelloWorld/Main.java
+++ JavaHelloWorld/src/HelloWorld/Main.java
@@ -7,5 +7,6 @@ public class Main
         System.out.println("Hello world");
         System.out.println("Test hello world");
         System.out.println("Line 3");
+        System.out.println("Line 4");
     }
 }
